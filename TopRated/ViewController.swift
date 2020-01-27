e332ee8d4c5fa059220fb4400567f94afa48efed
Antonio Arellano <jose.antonio.arellanomendoza@citi.com>
2020-01-27 08:25:18 -0600

    Initial commit

---
+++ TopRated/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TopRated
+//
+//  Created by José Antonio Arellano Mendoza on 1/27/20.
+//  Copyright © 2020 José Antonio Arellano Mendoza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ TopRatedTests/TopRatedTests.swift
@@ -0,0 +1,34 @@
+//
+//  TopRatedTests.swift
+//  TopRatedTests
+//
+//  Created by José Antonio Arellano Mendoza on 1/27/20.
+//  Copyright © 2020 José Antonio Arellano Mendoza. All rights reserved.
+//
+
+import XCTest
+@testable import TopRated
+
+class TopRatedTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
